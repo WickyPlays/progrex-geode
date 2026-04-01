61cd752301d5bc109ddf20fc32e921ad2039dc02
WickyPlays <nguyenthienbao8601@gmail.com>
2026-04-01 22:08:49 +0700

    Update for death point

--- src/main.cpp
+++ src/main.cpp
@@ -13,13 +13,14 @@ class $modify(PlayLayer) {
 
 	struct Fields {
 		std::vector<std::tuple<GameObject*, CCSprite*, CCSprite*>> m_coinStore;
+		std::vector<std::tuple<CCSprite*, CCSprite*>> m_deathStore;
 		std::vector<std::tuple<CheckpointObject*, CCSprite*>> m_checkpointStore;
 		int checkpointCount = 0;
+		bool gameplayStarted = false;
 	};
 
 	void rebuildCheckpoints() {
 
-		// remove old sprites from progress bar
 		for (auto [chk, sprite] : m_fields->m_checkpointStore) {
 			if (sprite) sprite->removeFromParentAndCleanup(true);
 		}
@@ -33,7 +34,8 @@ class $modify(PlayLayer) {
 
 			auto checkpoint = CCSprite::createWithSpriteFrameName("checkpoint_01_001.png");
 			checkpoint->setScale(0.35f);
-			checkpoint->setAnchorPoint({ 0.5f, 0.5f });
+			checkpoint->setAnchorPoint({ 0.5f,0.5f });
+
 			m_progressBar->addChild(checkpoint);
 
 			m_fields->m_checkpointStore.push_back({ chk, checkpoint });
@@ -42,7 +44,6 @@ class $modify(PlayLayer) {
 
 	void updateProg() {
 
-		// Coins sections
 		for (auto [obj, sprite, laneSprite] : m_fields->m_coinStore) {
 
 			auto newX = obj->getPositionX() / m_levelLength * m_progressBar->getContentWidth();
@@ -52,6 +53,7 @@ class $modify(PlayLayer) {
 			sprite->setPositionY(newY);
 
 			auto cache = CCSpriteFrameCache::sharedSpriteFrameCache();
+
 			sprite->setDisplayFrame(
 				cache->spriteFrameByName(fmt::format(
 					"secretCoin{}{}_01_001.png",
@@ -64,11 +66,12 @@ class $modify(PlayLayer) {
 			laneSprite->setPositionY(m_progressBar->getContentHeight() / 2);
 		}
 
-		//Checkpoint sections
 		int count = m_checkpointArray->count();
 
 		if (count != m_fields->checkpointCount) {
+
 			m_fields->checkpointCount = count;
+
 			rebuildCheckpoints();
 
 			for (auto [chk, sprite] : m_fields->m_checkpointStore) {
@@ -79,13 +82,13 @@ class $modify(PlayLayer) {
 				auto newX = obj->getPositionX() / m_levelLength * m_progressBar->getContentWidth();
 				auto newY = m_progressBar->getContentHeight() / 2;
 
-				sprite->setPositionX(newX);
-				sprite->setPositionY(newY);
+				sprite->setPosition({ newX,newY });
 			}
 		}
 	}
 
 	void setupHasCompleted() {
+
 		PlayLayer::setupHasCompleted();
 
 		for (auto& obj : CCArrayExt<GameObject>(m_objects)) {
@@ -97,10 +100,11 @@ class $modify(PlayLayer) {
 				m_progressBar->addChild(coin);
 
 				auto line = CCSprite::create("square.png");
-				line->setAnchorPoint({ 0.5f, 0.5f });
+				line->setAnchorPoint({ 0.5f,0.5f });
 				line->setScaleX(0.1f);
 				line->setScaleY(2.f);
 				line->setOpacity(200);
+
 				m_progressBar->addChild(line);
 
 				m_fields->m_coinStore.push_back({ obj, coin, line });
@@ -110,8 +114,43 @@ class $modify(PlayLayer) {
 		updateProg();
 	}
 
+	void destroyPlayer(PlayerObject * player, GameObject * gameObject) {
+
+		PlayLayer::destroyPlayer(player, gameObject);
+
+		if (!m_fields->gameplayStarted) {
+			m_fields->gameplayStarted = true;
+			log::info("Gameplay has started. Logging for ProgreX")
+			return;
+		}
+		else {
+			float percent = getCurrentPercent();
+
+			float newX = (percent / 100.f) * m_progressBar->getContentWidth();
+
+			auto skull = CCSprite::createWithSpriteFrameName("d_skull01_001.png");
+			skull->setScale(0.35f);
+			skull->setPosition({ newX,-5.f });
+
+			m_progressBar->addChild(skull);
+
+			auto line = CCSprite::create("square.png");
+			line->setAnchorPoint({ 0.5f, 0.5f });
+			line->setScaleX(0.1f);
+			line->setScaleY(2.f);
+			line->setOpacity(200);
+			line->setPosition({ newX, m_progressBar->getContentHeight() / 2 });
+
+			m_progressBar->addChild(line);
+
+			m_fields->m_deathStore.push_back({ skull,line });
+		}
+	}
+
 	void postUpdate(float dt) {
+
 		PlayLayer::postUpdate(dt);
+
 		updateProg();
 	}
 };
